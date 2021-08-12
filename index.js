a4b4d155de83d9acdd80f136be0d13d544bbf20d
Your Name <barcellinisilvia@gmail.com>
2021-08-12 17:21:17 +0200

    ✨ select random image

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-var randomNumber1 = Math.floor(Math.random() * 6) +1;
+var randomNumber1 = Math.floor(Math.random() * 6) +1; //select a number 1-6
 console.log(randomNumber1)
+
+var randomDice1 = "dice" + randomNumber1 + ".png"; 
