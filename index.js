57710bf87d1a7a1a0e55f151f8fa2018b633839a
Your Name <barcellinisilvia@gmail.com>
2021-08-12 17:23:01 +0200

    ✨ using concatenation

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var randomNumber1 = Math.floor(Math.random() * 6) +1; //select a number 1-6
 console.log(randomNumber1)
 
-var randomDice1 = "dice" + randomNumber1 + ".png"; 
+var randomDice1 = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
