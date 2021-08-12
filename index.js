5bdb8be1baeeec2e509e5149d9982f035c27f18d
Your Name <barcellinisilvia@gmail.com>
2021-08-12 17:43:02 +0200

    ✨ apply same logic to second dice

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ var randomNumber1 = Math.floor(Math.random() * 6) +1; //select a number 1-6
 console.log(randomNumber1)
 
 var randomDice1 = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
-
 var randomDiceSource = "images/" + randomDice1; //images/dice1.png - images/dice6.png
 
 var dice1 = document.querySelectorAll("img")[0]; //select first dice image in order to manipulate DOM 
-
 dice1.setAttribute("src", randomDiceSource); //update dynamically img src attribute 
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+console.log(randomNumber2)
+
+var randomDice2 = "dice" + randomNumber2 + ".png";
+var randomDiceSource2 = "images/" + randomDice2; 
+
+var dice2 = document.querySelectorAll("img")[1]; //select second dice image in order to manipulate DOM 
+dice2.setAttribute("src", randomDiceSource2); //update dynamically img src attribute 
