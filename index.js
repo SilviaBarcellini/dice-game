daffbed5f15828b3482eb814bc16be43e79822d5
Your Name <barcellinisilvia@gmail.com>
2021-08-12 17:14:46 +0200

    ♻️ shorten code

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
-var randomNumber1 = Math.random();
-randomNumber1 = randomNumber1 * 6; 
-randomNumber1 = Math.floor(randomNumber1) +1;  
+var randomNumber1 = Math.floor(Math.random() * 6) +1;
 console.log(randomNumber1)
